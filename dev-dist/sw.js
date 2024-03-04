0d8a54deb5339d1a2179bb6b5edc3527d2cf5974
JuliaOliveira26 <julinhalves2006@gmail.com>
2024-03-04 11:56:56 -0300

    aplicação

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.st9ni74373"
+    "revision": "0.m70jumfa6ig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
